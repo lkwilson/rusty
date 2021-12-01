39de4c4088e5e06feef0176d3da5d8de0f110215
Lucas Wilson <lkwilson96@gmail.com>
2021-11-30 22:44:12 -0700

    Dynamic sub module loading

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,22 @@
 mod sub_mod;
 
-fn main() {
-  let exit_code = sub_mod::main();
+fn run() -> u8 {
+  let args: Vec<String> = std::env::args().collect();
+  if args.len() != 2 {
+    println!("Usage: {} <sub_module>", args[0]);
+    return 1;
+  }
+  match args[1].as_str() {
+    "sub_mod" => sub_mod::main(),
+    _ => {
+      println!("Unknown module: {}", args[1]);
+      1
+    }
+  }
+}
 
+fn main() {
+  let exit_code = run();
   println!("Module exited with code {}", exit_code);
   std::process::exit(exit_code.into());
 }
