b7b0793e9eba36f07e4d6d52ad7c0c613a3812f6
Lucas Wilson <lkwilson96@gmail.com>
2021-12-05 10:23:31 -0700

    Add heap sort

--- src/heaps/heap.rs
+++ src/heaps/heap.rs
@@ -15,14 +15,15 @@ pub fn get_left_child_index(node_index: usize) -> usize {
 /**
  * This will heapify a vector from index node_index with heap property tester,
  * compare. compare(parent_value, child_value) will return true if the heap
- * property for those nodes is satisfied. This function assumes child nodes are
- * heaps, but the node at node_index may not be.
+ * property for those nodes is satisfied. note that this means compare(value,
+ * value) is always true. This function assumes child nodes are heaps, but the
+ * node at node_index may not be. size is usually vec.len(), but doesn't have to
+ * be if you want to use the back elements, for say heap sort.
  */
-fn heapify_from<T, F>(vec: &mut Vec<T>, compare: &F, node_index: usize) where
+fn heapify_from<T, F>(vec: &mut Vec<T>, compare: &F, node_index: usize, size: usize) where
     F: Fn(&T, &T)->bool {
   let li = get_left_child_index(node_index);
   let ri = li + 1;
-  let size = vec.len();
 
   let mut swap_index = None;
   let nv = &vec[node_index];
@@ -42,7 +43,7 @@ fn heapify_from<T, F>(vec: &mut Vec<T>, compare: &F, node_index: usize) where
   match swap_index {
     Some(swap_index) => {
       vec.swap(swap_index, node_index);
-      heapify_from(vec, compare, swap_index);
+      heapify_from(vec, compare, swap_index, size);
     },
     None => {}
   }
@@ -62,20 +63,29 @@ fn heapify_from<T, F>(vec: &mut Vec<T>, compare: &F, node_index: usize) where
 // fn heapify_from<T, F, const HEAPIFY_CHILDREN: bool>(vec: &mut Vec<T>, compare: &F, node_index: usize)
 // where F: Fn(&T, &T)->bool, T: std::fmt::Debug {...}
 
-fn heapify<F, T>(vec: &mut Vec<T>, compare: &F)
-where F:Fn(&T, &T)->bool, T:std::fmt::Debug {
-  let size = vec.len();
+fn heapify<F, T>(vec: &mut Vec<T>, compare: &F, size: usize)
+where F:Fn(&T, &T)->bool {
   if size > 1 {
     let index = size - 1;
     let index = get_parent_index(index);
     for index in (0..(index+1)).rev() {
-      heapify_from(vec, compare, index);
+      heapify_from(vec, compare, index, size);
     }
   }
 }
 
+fn heap_sort<F, T>(vec: &mut Vec<T>, compare: &F) where
+    F: Fn(&T, &T)->bool {
+  heapify(vec, compare, vec.len());
+  let size = vec.len();
+  for index in (1..size).rev() {
+    vec.swap(0, index);
+    heapify_from(vec, compare, 0, index);
+  }
+}
+
 pub fn is_heap<F, T>(vec: &Vec<T>, compare: &F) -> bool
-where F: Fn(&T, &T)->bool, T: std::fmt::Debug {
+where F: Fn(&T, &T)->bool {
   for (index, node) in vec.iter().enumerate() {
     if index == 0 {
       continue;
@@ -88,19 +98,40 @@ where F: Fn(&T, &T)->bool, T: std::fmt::Debug {
   true
 }
 
+pub fn is_sorted<T, F>(vec: &Vec<T>, compare: &F) -> bool
+where F: Fn(&T, &T)->bool {
+  for index in 1..vec.len() {
+    let prev_value = &vec[index-1];
+    let curr_value = &vec[index];
+    if !compare(prev_value, curr_value) {
+      return false;
+    }
+  }
+  true
+}
+
 pub fn main() -> u8 {
   let mut heap = vec![0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5];
+  let size = heap.len();
   println!("Pre heapify: {:?}", heap);
 
-  heapify(&mut heap, &std::cmp::PartialOrd::ge);
+  heapify(&mut heap, &std::cmp::PartialOrd::ge, size);
   println!("Post max heapify: {:?}", heap);
   assert!(is_heap(&heap, &std::cmp::PartialOrd::ge));
   assert!(!is_heap(&heap, &std::cmp::PartialOrd::le));
 
-  heapify(&mut heap, &std::cmp::PartialOrd::le);
+  heapify(&mut heap, &std::cmp::PartialOrd::le, size);
   println!("Post min heapify: {:?}", heap);
   assert!(is_heap(&heap, &std::cmp::PartialOrd::le));
   assert!(!is_heap(&heap, &std::cmp::PartialOrd::ge));
+
+  heap_sort(&mut heap, &std::cmp::PartialOrd::ge);
+  println!("Post max heap sort: {:?}", heap);
+  assert!(is_sorted(&mut heap, &std::cmp::PartialOrd::le));
+
+  heap_sort(&mut heap, &std::cmp::PartialOrd::le);
+  println!("Post min heap sort: {:?}", heap);
+  assert!(is_sorted(&mut heap, &std::cmp::PartialOrd::ge));
 
   0
 }
