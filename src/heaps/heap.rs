440f18b567af7f74f9e3f822b3327e2b406e847f
Lucas Wilson <lkwilson96@gmail.com>
2021-12-05 09:28:24 -0700

    Optimize heapify

--- src/heaps/heap.rs
+++ src/heaps/heap.rs
@@ -13,23 +13,16 @@ pub fn get_left_child_index(node_index: usize) -> usize {
 }
 
 /**
- * Vec must be non empty, and index must be in range. Otherwise, undefined behavior.
- *
- * compare(parent, child) must return true, if it meets the heap property.
+ * This will heapify a vector from index node_index with heap property tester,
+ * compare. compare(parent_value, child_value) will return true if the heap
+ * property for those nodes is satisfied. This function assumes child nodes are
+ * heaps, but the node at node_index may not be.
  */
-fn heapify_from<T, F, const HEAPIFY_CHILDREN: bool>(vec: &mut Vec<T>, compare: &F, node_index: usize)
-where F: Fn(&T, &T)->bool, T: std::fmt::Debug {
+fn heapify_from<T, F>(vec: &mut Vec<T>, compare: &F, node_index: usize) where
+    F: Fn(&T, &T)->bool {
   let li = get_left_child_index(node_index);
   let ri = li + 1;
   let size = vec.len();
-  if HEAPIFY_CHILDREN {
-    if ri < size {
-      heapify_from::<_, _, true>(vec, compare, li);
-      heapify_from::<_, _, true>(vec, compare, ri);
-    } else if li < size {
-      heapify_from::<_, _, true>(vec, compare, li);
-    }
-  }
 
   let mut swap_index = None;
   let nv = &vec[node_index];
@@ -49,16 +42,35 @@ where F: Fn(&T, &T)->bool, T: std::fmt::Debug {
   match swap_index {
     Some(swap_index) => {
       vec.swap(swap_index, node_index);
-      heapify_from::<_, _, false>(vec, compare, swap_index);
+      heapify_from(vec, compare, swap_index);
     },
     None => {}
   }
 }
 
+/**
+ * Vec must be non empty, and index must be in range. Otherwise, undefined behavior.
+ *
+ * compare(parent, child) must return true, if it meets the heap property.
+ *
+ * This function was inefficient for lots of reasons:
+ * - we use recursion to get to the deepest leaves
+ * - we heapify the leaf nodes when we know exactly where the leaf nodes are
+ * - when ensuring children are heaps, we start from the root when we can jump
+ *   straight to the parent of the last leaf node
+ */
+// fn heapify_from<T, F, const HEAPIFY_CHILDREN: bool>(vec: &mut Vec<T>, compare: &F, node_index: usize)
+// where F: Fn(&T, &T)->bool, T: std::fmt::Debug {...}
+
 fn heapify<F, T>(vec: &mut Vec<T>, compare: &F)
 where F:Fn(&T, &T)->bool, T:std::fmt::Debug {
-  if !vec.is_empty() {
-    heapify_from::<_, _, true>(vec, compare, 0);
+  let size = vec.len();
+  if size > 1 {
+    let index = size - 1;
+    let index = get_parent_index(index);
+    for index in (0..(index+1)).rev() {
+      heapify_from(vec, compare, index);
+    }
   }
 }
 
