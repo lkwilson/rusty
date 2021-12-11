7f072d78a77cf140558a7f170b572554d992cf79
Lucas Wilson <lkwilson96@gmail.com>
2021-12-11 09:34:39 -0700

    Remove sub mods

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,2 @@
 pub mod heap_lib;
-pub mod sub_mod;
-pub mod sub_mod_file;
 pub mod play;
--- src/sub_mod_file.rs
+++
@@ -1,7 +0,0 @@
-#[cfg(test)]
-mod tests {
-  #[test]
-  fn run() {
-    println!("I'm a submodule, but a file");
-  }
-}
